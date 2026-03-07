0389a1128663dd13fbcb1e6cab631c7354e6c00b
Choi Yong Hoon <dydgns304@naver.com>
2026-03-07 21:59:31 +0900

    commit

---
+++ level0/05/05.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+
+int main()
+{
+    std::cout << R"(!@#$ % ^&*(\'"<>?:;)";
+    
+    return 0;
+}
+
---
+++ level0/08/08.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str;
+    cin >> str;
+    for (int i = 0; i < str.size(); i++){
+		printf("%c\n", str[i]);
+    }
+    return 0;
+}
